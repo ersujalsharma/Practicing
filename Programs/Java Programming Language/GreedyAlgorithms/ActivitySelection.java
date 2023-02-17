1c295d54982ca29b8cdd2953712dbc5c271ef686
sujalsh <sujalsharma350@gmail.com>
2023-02-17 16:08:12 +0530

    actvitySelectionSolved

--- Programs/Java Programming Language/GreedyAlgorithms/ActivitySelection.java
+++ Programs/Java Programming Language/GreedyAlgorithms/ActivitySelection.java
@@ -1,5 +1,22 @@
 package GreedyAlgorithms;
 
 public class ActivitySelection {
-    
+    public static void main(String[] args) {
+        int[] start = {10,12,20};// {1,3,0,5,8,5};
+        int[] end ={20,25,30};// {2,4,6,7,9,9}; // sorted
+        int ans = countActivity(start,end);
+        System.out.println(ans);
+    }
+
+    private static int countActivity(int[] start, int[] end) {
+        int count =1;
+        int prevEnd = end[0];
+        for (int i = 1; i < end.length; i++) {
+            if(start[i]>=prevEnd){ // non overlapping
+                count++;
+                prevEnd = end[i];
+            }
+        }
+        return count;
+    }
 }
