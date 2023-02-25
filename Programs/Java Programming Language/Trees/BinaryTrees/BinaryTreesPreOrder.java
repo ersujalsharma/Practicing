e2e8ececb4a8af198715e3cc99a012cd353276b9
sujalsh <sujalsharma350@gmail.com>
2023-02-25 15:41:46 +0530

    vg

--- Programs/Java Programming Language/Trees/BinaryTrees/BinaryTreesPreOrder.java
+++ Programs/Java Programming Language/Trees/BinaryTrees/BinaryTreesPreOrder.java
@@ -1,5 +1,9 @@
 package Trees.BinaryTrees;
 
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTreesPreOrder {
     static class BinaryTree{
         static int index = -1;
@@ -28,7 +32,53 @@ public class BinaryTreesPreOrder {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree b = new BinaryTree();
         Node root = b.buildTree(nodes);
-        preOrder(root);
+        // preOrder(root);
+        // inOrder(root);
+        // postOrder(root);
+        levelOrder(root);
+    }
+    private static void levelOrder(Node root) {
+        if(root ==null){
+            return;
+        }
+        Queue<Node> queue = new Arra<>();
+        queue.add(root);
+        queue.add(null);
+        while(!queue.isEmpty()){
+            Node currentNode = queue.remove();
+            if(currentNode==null){
+                System.out.println();
+                if(queue.isEmpty()){
+                    break;
+                }
+                else{
+                    queue.add(null);
+                }
+            }
+            else{
+                System.out.println(currentNode.data);
+                if(currentNode.left!=null)
+                queue.add(currentNode.left);
+                if(currentNode.right!=null);
+                queue.add(currentNode.right);
+            }
+        }
+    }
+    private static void postOrder(Node root) {
+        if(root==null){
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.println(root.data);
+    }
+    private static void inOrder(Node root) {
+        if(root==null){
+            return;
+        }
+        inOrder(root.left);
+        System.out.println(root.data);
+        inOrder(root.right);
     }
     private static void preOrder(Node root) {
         if(root==null){
